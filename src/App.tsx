0f526d533eeed956d0d300fcd8c187ed513314f3
横島 健一 <yokoshima-kenichi@alldmm.com>
2020-01-23 19:03:30 +0900

    Add dropzone

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,22 @@
 import React from 'react';
-import Table from './Table';
-
+import Products from './Products';
+import Dropzone, { useDropzone } from 'react-dropzone'
 
 
 const App: React.FC = () => {
   return (
     <div>
-      <Table />
+      <Products />
+      <Dropzone onDrop={acceptedFiles => console.log(acceptedFiles)}>
+        {({ getRootProps, getInputProps }) => (
+          <section>
+            <div {...getRootProps()}>
+              <input {...getInputProps()} />
+              <p>Drag 'n' drop some files here, or click to select files</p>
+            </div>
+          </section>
+        )}
+      </Dropzone>
     </div>
   );
 }
---
+++ src/Products.tsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import { Table, TableContainer, TableHead, TableRow, TableCell, TableBody } from '@material-ui/core'
+
+export default function Products() {
+  return (
+    <TableContainer>
+      <Table>
+        <TableHead>
+          <TableRow>
+            <TableCell></TableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          <TableRow>
+            <TableCell></TableCell>
+          </TableRow>
+        </TableBody>
+      </Table>
+    </TableContainer>
+  )
+}
