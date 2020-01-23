515a97e265e407db2e6c0c56cdd8770f7329bdea
kyokoshima <dev.kyoko@gmail.com>
2020-01-23 21:31:41 +0900

    Add dropzone

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,34 @@
 import React from 'react';
 import Products from './Products';
 import Dropzone, { useDropzone } from 'react-dropzone'
+import { useCallback } from 'react';
 
 
 const App: React.FC = () => {
+  const onDrop = useCallback((acceptedFiles) => {
+    acceptedFiles.forEach((file: Blob) => {
+      const reader = new FileReader()
+      reader.onload = async () => {
+        console.log('load')
+        
+        console.log(reader.result)
+        console.log(await reader.readAsText)
+      }
+      console.log('no load')
+      const content = reader.readAsText(file)
+      console.log(content)
+    })
+  }, [])
+  const {getRootProps, getInputProps} = useDropzone({onDrop})
   return (
     <div>
       <Products />
-      <Dropzone onDrop={acceptedFiles => console.log(acceptedFiles)}>
-        {({ getRootProps, getInputProps }) => (
-          <section>
-            <div {...getRootProps()}>
-              <input {...getInputProps()} />
-              <p>Drag 'n' drop some files here, or click to select files</p>
-            </div>
-          </section>
-        )}
-      </Dropzone>
+      <section>
+        <div {...getRootProps()}>
+          <input {...getInputProps()} />
+          <p>Drag 'n' drop some files here, or click to select files</p>
+        </div>
+      </section>
     </div>
   );
 }
